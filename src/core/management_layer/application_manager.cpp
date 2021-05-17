b8f15ee0ab0fc25c265f06c6b4cde84c321eda99
Marshev M.S <msmarshev@gmail.com>
2021-05-18 00:22:04 +0400

    notify layout direction change

--- src/core/management_layer/application_manager.cpp
+++ src/core/management_layer/application_manager.cpp
@@ -586,8 +586,10 @@ void ApplicationManager::Implementation::setTranslation(QLocale::Language _langu
     if (currentLanguage == QLocale::Persian
         || currentLanguage == QLocale::Hebrew) {
         QApplication::setLayoutDirection(Qt::RightToLeft);
+        QApplication::postEvent(q, new DesignSystemChangeEvent);
     } else {
         QApplication::setLayoutDirection(Qt::LeftToRight);
+        QApplication::postEvent(q, new DesignSystemChangeEvent);
     }
 
     //
