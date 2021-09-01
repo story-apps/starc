b16d48381ce93ebc85addb8f363c781f09ec19c9
dimkanovikov <to@dimkanovikov.pro>
2021-09-01 19:50:10 +0300

    Implement AppImage building in CI

---
+++ build/linux/make-app-image.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+#
+# $1 - application version
+#
+
+#
+# Prepare folders structure
+#
+APP_IMAGE_DIR="appdir"
+mkdir $APP_IMAGE_DIR/lib
+mkdir $APP_IMAGE_DIR/plugins
+ls -l $APP_IMAGE_DIR
+
+#
+# Copy application binaries
+#
+APP_BIN_DIR="../../src/_build"
+ls -l $APP_BIN_DIR
+cp $APP_BIN_DIR/starcapp $APP_IMAGE_DIR/starc
+cp $APP_BIN_DIR/libcorelib.so.1 $APP_IMAGE_DIR/lib/
+cp $APP_BIN_DIR/plugins/*.so $APP_IMAGE_DIR/plugins/
+
+#
+# Make installer
+#
+wget -c -nv "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage" -O linuxdeployqt
+chmod a+x linuxdeployqt
+./linuxdeployqt appdir/starc -no-copy-copyright-files -no-translations -always-overwrite -executable=appdir/plugins/libcoreplugin.so -appimage
+mv *.AppImage starc-$1.AppImage
