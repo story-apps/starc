cbf52e7dc7886b165571dbe0b1ea4ccbedb26be2
dimkanovikov <to@dimkanovikov.pro>
2021-10-18 07:24:38 +0300

    Add dmg app signing

--- build/mac/create_dmg.sh
+++ build/mac/create_dmg.sh
@@ -3,13 +3,20 @@
 #
 # сформировать app-файл
 #
-cp ../../src/_build/starcapp.app starc.app
-$Qt5_Dir/bin/macdeployqt starcapp.app
+cp -R ../../src/_build/starcapp.app starcapp.app
+macdeployqt starcapp.app
 
 #
 # подпишем app-файл
 #
-codesign --force --entitlements entitlements.plist --deep --sign "$APPLE_SIGNING_IDENTITY" --options "runtime" "starcapp.app/"
+echo $APPLE_CERTIFICATE | base64 --decode > certificate.p12
+security create-keychain -p $APPLE_KEYCHAIN_PASSWORD build.keychain
+security default-keychain -s build.keychain
+security unlock-keychain -p $APPLE_KEYCHAIN_PASSWORD build.keychain
+security import certificate.p12 -k build.keychain -P $APPLE_CERTIFICATE_PASSWORD -T /usr/bin/codesign
+security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k $APPLE_KEYCHAIN_PASSWORD build.keychain
+security find-identity
+codesign --force --entitlements entitlements.plist --deep --sign "$APPLE_SIGNING_IDENTITY" --options "runtime" "starcapp.app/" -v
 
 #
 # сформировать архив с приложением
@@ -19,7 +26,7 @@ ditto -ck --keepParent starcapp.app starcapp.zip
 #
 # отправить архив на проверку
 #
-xcrun notarytool submit starcapp.zip --apple-id $APPLE_ID --password $APPLE_PASSWORD --team-id $APPLE_TEAM_ID --wait
+xcrun notarytool submit starcapp.zip --apple-id $APPLE_ID --password $APPLE_ID_APP_PASSWORD --team-id $APPLE_TEAM_ID --wait
 
 #
 # удалить архив
