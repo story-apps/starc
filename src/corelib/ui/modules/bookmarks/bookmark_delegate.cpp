676a0f1bbecb56aa2751f6e20701e2092f78f34f
dimkanovikov <to@dimkanovikov.pro>
2022-05-27 07:46:37 +0300

    Adopt bookmarks delegate to the RTL

--- src/corelib/ui/modules/bookmarks/bookmark_delegate.cpp
+++ src/corelib/ui/modules/bookmarks/bookmark_delegate.cpp
@@ -39,6 +39,7 @@ void BookmarkDelegate::paint(QPainter* _painter, const QStyleOptionViewItem& _op
 
     auto backgroundColor = opt.palette.color(QPalette::Base);
     auto textColor = opt.palette.color(QPalette::Text);
+    const auto isLeftToRight = QLocale().textDirection() == Qt::LeftToRight;
 
     //
     // Рисуем
@@ -70,41 +71,65 @@ void BookmarkDelegate::paint(QPainter* _painter, const QStyleOptionViewItem& _op
     //
     // ... цвет закладки
     //
-    const QRectF colorRect(QPointF(0.0, backgroundRect.top()),
-                           QSizeF(Ui::DesignSystem::layout().px4(), backgroundRect.height()));
-    _painter->fillRect(colorRect, _index.data(BookmarksModel::BookmarkColorRole).value<QColor>());
+    const auto itemColor = _index.data(BookmarksModel::BookmarkColorRole);
+    QRectF itemColorRect;
+    if (!itemColor.isNull() && itemColor.canConvert<QColor>()) {
+        const QColor color = itemColor.value<QColor>();
+        if (color.isValid()) {
+            auto fullIndicatorWidth = [_index] {
+                int level = 0;
+                auto index = _index;
+                while (index.isValid()) {
+                    ++level;
+                    index = index.parent();
+                }
+                return level * Ui::DesignSystem::tree().indicatorWidth();
+            };
+            const auto backgroundRect = _option.rect;
+            itemColorRect = QRectF(isLeftToRight ? 0.0
+                                                 : (backgroundRect.right() + fullIndicatorWidth()
+                                                    - Ui::DesignSystem::layout().px4()),
+                                   backgroundRect.top(), Ui::DesignSystem::layout().px4(),
+                                   backgroundRect.height());
+            _painter->fillRect(itemColorRect, color);
+        }
+    }
 
     //
     // ... заголовок
     //
     _painter->setFont(Ui::DesignSystem::font().subtitle2());
     _painter->setPen(textColor);
-    const qreal textLeft = colorRect.right() + Ui::DesignSystem::layout().px16();
-    const qreal textWidth = backgroundRect.right() - textLeft - Ui::DesignSystem::layout().px12();
-    const QRectF headerRect(
+    const qreal textLeft = isLeftToRight
+        ? (itemColorRect.right() + Ui::DesignSystem::layout().px16())
+        : Ui::DesignSystem::layout().px12();
+    const qreal textWidth = isLeftToRight
+        ? (backgroundRect.right() - textLeft - Ui::DesignSystem::layout().px12())
+        : (backgroundRect.right() + Ui::DesignSystem::tree().indicatorWidth() - textLeft
+           - itemColorRect.width() - Ui::DesignSystem::layout().px16());
+    const QRectF headingRect(
         QPointF(textLeft, backgroundRect.top() + Ui::DesignSystem::layout().px16()),
         QSizeF(textWidth, Ui::DesignSystem::treeOneLineItem().iconSize().height()));
     auto header
         = TextHelper::smartToUpper(_index.data(BookmarksModel::BookmarkItemTextRole).toString());
     header = _painter->fontMetrics().elidedText(header, Qt::ElideRight,
-                                                static_cast<int>(headerRect.width()));
-    _painter->drawText(headerRect, Qt::AlignLeft | Qt::AlignVCenter, header);
+                                                static_cast<int>(headingRect.width()));
+    _painter->drawText(headingRect, Qt::AlignLeft | Qt::AlignVCenter, header);
 
     //
     // ... текст закладки
     //
     const auto bookmarkName = _index.data(BookmarksModel::BookmarkNameRole).toString();
     if (!bookmarkName.isEmpty()) {
-        const auto bookmarkNameWidth = backgroundRect.right() - Ui::DesignSystem::layout().px16()
-            - Ui::DesignSystem::layout().px16() - Ui::DesignSystem::layout().px8();
         const QRectF bookamrkNameRect(
-            QPointF(headerRect.left(), headerRect.bottom() + Ui::DesignSystem::layout().px4()),
-            QSizeF(bookmarkNameWidth,
+            QPointF(headingRect.left(), headingRect.bottom() + Ui::DesignSystem::layout().px4()),
+            QSizeF(headingRect.width(),
                    TextHelper::heightForWidth(bookmarkName, Ui::DesignSystem::font().body2(),
-                                              bookmarkNameWidth)));
+                                              headingRect.width())));
         _painter->setFont(Ui::DesignSystem::font().body2());
         _painter->setPen(textColor);
         QTextOption commentTextOption;
+        commentTextOption.setAlignment(isLeftToRight ? Qt::AlignLeft : Qt::AlignRight);
         commentTextOption.setWrapMode(QTextOption::WrapAtWordBoundaryOrAnywhere);
         _painter->drawText(bookamrkNameRect, bookmarkName, commentTextOption);
     }
