10e73deb159b759e763992da217b9063f61f191f
dimkanovikov <to@dimkanovikov.pro>
2022-07-20 13:31:29 +0300

    Fix showing version color when user deside to edit version

--- src/core/ui/project/create_version_dialog.cpp
+++ src/core/ui/project/create_version_dialog.cpp
@@ -98,6 +98,7 @@ void CreateVersionDialog::edit(const QString& _name, const QColor& _color, bool 
     updateTranslations();
 
     d->versionName->setText(_name);
+    d->versionName->setTrailingIconColor(_color);
     d->versionColorPopup->setSelectedColor(_color);
     d->allowEditVersion->setChecked(!_readOnly);
     d->allowEditVersion->setEnabled(false);
