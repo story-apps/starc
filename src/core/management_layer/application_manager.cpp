73417808cc36b351ff10eaff18e275fbb3569968
dimkanovikov <to@dimkanovikov.pro>
2023-04-21 11:27:49 +0300

    Bump version number to 0.5.1 again :)

--- src/core/management_layer/application_manager.cpp
+++ src/core/management_layer/application_manager.cpp
@@ -2132,7 +2132,7 @@ ApplicationManager::ApplicationManager(QObject* _parent)
     Log::init(loggingLevel, logFilePath);
 
 
-    QString applicationVersion = "0.5.0a";
+    QString applicationVersion = "0.5.1";
 #if defined(DEV_BUILD) && DEV_BUILD > 0
     applicationVersion += QString(" dev %1").arg(DEV_BUILD);
 #endif
