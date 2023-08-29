7734a50d7bc58738a6c90e1770eb0fe33a0d2256
dimkanovikov <to@dimkanovikov.pro>
2023-08-29 06:42:17 +0300

    Fix issue with cursor position restoring for cloud projects

--- src/core/management_layer/plugins/simple_text/simple_text_view.cpp
+++ src/core/management_layer/plugins/simple_text/simple_text_view.cpp
@@ -133,6 +133,7 @@ public:
     SimpleTextEdit* textEdit = nullptr;
     SimpleTextEditShortcutsManager shortcutsManager;
     ScalableWrapper* scalableWrapper = nullptr;
+    std::optional<int> pendingCursorPosition;
 
     //
     // Панели инструментов
@@ -1074,6 +1075,10 @@ void SimpleTextView::saveViewSettings()
 
 void SimpleTextView::setModel(BusinessLayer::SimpleTextModel* _model)
 {
+    if (d->model) {
+        d->model->disconnect(this);
+    }
+
     d->model = _model;
 
     //
@@ -1082,6 +1087,35 @@ void SimpleTextView::setModel(BusinessLayer::SimpleTextModel* _model)
     if (d->model) {
         const bool reinitModel = true;
         d->reconfigureTemplate(!reinitModel);
+
+        //
+        // Перед началом сброса документа запоминаем текущую позицию курсора
+        //
+        connect(d->model, &BusinessLayer::SimpleTextModel::modelAboutToBeReset, this, [this] {
+            if (!d->pendingCursorPosition.has_value()) {
+                d->pendingCursorPosition = cursorPosition();
+            }
+        });
+        //
+        // ... после завершения сброса, отложенно возвращаем курсор на место
+        //
+        connect(
+            d->model, &BusinessLayer::SimpleTextModel::modelReset, this,
+            [this] {
+                //
+                // Извлечём позицию для установки
+                //
+                const int position = d->pendingCursorPosition.value();
+                //
+                // ... затем сбрасываем буфер, чтобы позиция установилась внутрь редактора текста
+                //
+                d->pendingCursorPosition.reset();
+                //
+                // ... устанавливаем позицию в редактор
+                //
+                setCursorPosition(position);
+            },
+            Qt::QueuedConnection);
     }
 
     d->textEdit->setCursors({});
@@ -1104,6 +1138,11 @@ int SimpleTextView::cursorPosition() const
 
 void SimpleTextView::setCursorPosition(int _position)
 {
+    if (d->pendingCursorPosition.has_value()) {
+        d->pendingCursorPosition = _position;
+        return;
+    }
+
     auto cursor = d->textEdit->textCursor();
     cursor.setPosition(_position);
     d->textEdit->ensureCursorVisible(cursor, false);
