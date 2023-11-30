9e99924bb9f6e9e1943923e7a0a3f7bc633acd5b
dimkanovikov <to@dimkanovikov.pro>
2023-11-30 11:24:09 +0300

    Fix issue with novel editor text area width for plain text mode

--- src/core/management_layer/plugins/novel_text/novel_text_view.cpp
+++ src/core/management_layer/plugins/novel_text/novel_text_view.cpp
@@ -415,10 +415,7 @@ void NovelTextView::Implementation::updateTextEditPageMargins()
     }
 
     const QMarginsF pageMargins
-        = QMarginsF{ 15, 20 / scalableWrapper->zoomRange(),
-                     12 / scalableWrapper->zoomRange()
-                         + MeasurementHelper::pxToMm(scalableWrapper->verticalScrollBar()->width()),
-                     5 };
+        = QMarginsF{ 15, 20 / scalableWrapper->zoomRange(), 12 / scalableWrapper->zoomRange(), 5 };
     textEdit->setPageMarginsMm(pageMargins);
 }
 
