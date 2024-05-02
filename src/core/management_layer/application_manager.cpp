04ee3fb470714594b086c4ede2838ab76eee7bf0
dimkanovikov <to@dimkanovikov.pro>
2024-05-02 16:34:06 +0300

    Bump version number to 0.7.2

--- src/core/management_layer/application_manager.cpp
+++ src/core/management_layer/application_manager.cpp
@@ -2185,7 +2185,7 @@ ApplicationManager::ApplicationManager(QObject* _parent)
 #endif
     Log::init(loggingLevel, logFilePath);
 
-    QString applicationVersion = "0.7.1";
+    QString applicationVersion = "0.7.2";
 #if defined(DEV_BUILD) && DEV_BUILD > 0
     applicationVersion += QString(" dev %1").arg(DEV_BUILD);
 #endif
