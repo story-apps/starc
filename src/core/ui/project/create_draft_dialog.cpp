d0e79cf66db1a12cbd6eced7a71a895692d8f866
dimkanovikov <to@dimkanovikov.pro>
2024-10-06 14:32:36 +0300

    Improve create/edit draft dialog

--- src/core/ui/project/create_draft_dialog.cpp
+++ src/core/ui/project/create_draft_dialog.cpp
@@ -35,7 +35,7 @@ public:
     ColorPickerPopup* versionColorPopup = nullptr;
     ComboBox* sourceVersion = nullptr;
     QStringListModel* sourceVersionModel = nullptr;
-    CheckBox* allowEditVersion = nullptr;
+    CheckBox* lockEditingVersion = nullptr;
 
     QHBoxLayout* buttonsLayout = nullptr;
     Button* cancelButton = nullptr;
@@ -49,7 +49,7 @@ CreateDraftDialog::Implementation::Implementation(QWidget* _parent)
     , versionColorPopup(new ColorPickerPopup(_parent))
     , sourceVersion(new ComboBox(_parent))
     , sourceVersionModel(new QStringListModel(sourceVersion))
-    , allowEditVersion(new CheckBox(_parent))
+    , lockEditingVersion(new CheckBox(_parent))
     , buttonsLayout(new QHBoxLayout)
     , cancelButton(new Button(_parent))
     , createButton(new Button(_parent))
@@ -60,6 +60,7 @@ CreateDraftDialog::Implementation::Implementation(QWidget* _parent)
     versionName->setTrailingIcon(u8"\U000F0765");
     versionName->setTrailingIconColor(versionColorPopup->selectedColor());
     sourceVersion->setModel(sourceVersionModel);
+    lockEditingVersion->setChecked(false);
     createButton->setEnabled(false);
 
     buttonsLayout->setContentsMargins({});
@@ -84,7 +85,7 @@ CreateDraftDialog::CreateDraftDialog(QWidget* _parent)
     int row = 0;
     contentsLayout()->addWidget(d->versionName, row++, 0);
     contentsLayout()->addWidget(d->sourceVersion, row++, 0);
-    contentsLayout()->addWidget(d->allowEditVersion, row++, 0);
+    contentsLayout()->addWidget(d->lockEditingVersion, row++, 0);
     contentsLayout()->addLayout(d->buttonsLayout, row++, 0);
 
     connect(d->versionName, &TextField::textChanged, this,
@@ -96,7 +97,8 @@ CreateDraftDialog::CreateDraftDialog(QWidget* _parent)
             [this](const QColor& _color) { d->versionName->setTrailingIconColor(_color); });
     connect(d->createButton, &Button::clicked, this, [this] {
         emit savePressed(d->versionName->text(), d->versionColorPopup->selectedColor(),
-                         d->sourceVersion->currentIndex().row(), !d->allowEditVersion->isChecked());
+                         d->sourceVersion->currentIndex().row(),
+                         d->lockEditingVersion->isChecked());
     });
     connect(d->cancelButton, &Button::clicked, this, &CreateDraftDialog::hideDialog);
 }
@@ -120,7 +122,7 @@ void CreateDraftDialog::edit(const QString& _name, const QColor& _color, bool _r
     d->versionName->setTrailingIconColor(_color);
     d->sourceVersion->hide();
     d->versionColorPopup->setSelectedColor(_color);
-    d->allowEditVersion->setChecked(!_readOnly);
+    d->lockEditingVersion->setChecked(_readOnly);
 }
 
 QWidget* CreateDraftDialog::focusedWidgetAfterShow() const
@@ -139,7 +141,7 @@ void CreateDraftDialog::updateTranslations()
 
     d->versionName->setLabel(tr("Draft name"));
     d->sourceVersion->setLabel(tr("New draft based on"));
-    d->allowEditVersion->setText(tr("Allow to edit draft"));
+    d->lockEditingVersion->setText(tr("Lock draft text editing"));
     d->cancelButton->setText(tr("Cancel"));
     d->createButton->setText(d->state == AddNew ? tr("Create") : tr("Save"));
 }
@@ -158,8 +160,8 @@ void CreateDraftDialog::designSystemChangeEvent(DesignSystemChangeEvent* _event)
     d->sourceVersion->setCustomMargins({ Ui::DesignSystem::layout().px24(),
                                          Ui::DesignSystem::layout().px12(),
                                          Ui::DesignSystem::layout().px24(), 0.0 });
-    d->allowEditVersion->setTextColor(Ui::DesignSystem::color().onBackground());
-    d->allowEditVersion->setBackgroundColor(Ui::DesignSystem::color().background());
+    d->lockEditingVersion->setTextColor(Ui::DesignSystem::color().onBackground());
+    d->lockEditingVersion->setBackgroundColor(Ui::DesignSystem::color().background());
 
     for (auto button : { d->cancelButton, d->createButton }) {
         button->setBackgroundColor(Ui::DesignSystem::color().accent());
