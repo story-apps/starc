8e27e0ce96a9de8b988cca2513da4eaf1de3ad24
Ilya Putilin <putilya16@gmail.com>
2025-03-31 14:39:32 +0700

    Refactor presentation module for async DB access

--- src/corelib/business_layer/model/presentation/presentation_model.h
+++ src/corelib/business_layer/model/presentation/presentation_model.h
@@ -2,10 +2,6 @@
 
 #include "../abstract_model.h"
 
-namespace Domain {
-struct DocumentImage;
-}
-
 namespace BusinessLayer {
 
 /**
@@ -19,18 +15,49 @@ public:
     explicit PresentationModel(QObject* _parent = nullptr);
     ~PresentationModel() override;
 
+    /**
+     * @brief Имя презентации
+     */
     QString documentName() const override;
     void setDocumentName(const QString& _name) override;
     Q_SIGNAL void nameChanged(const QString& _name);
 
+    /**
+     * @brief Описание презентации
+     */
     QString description() const;
     void setDescription(const QString& _description);
     Q_SIGNAL void descriptionChanged(const QString& _description);
 
-    QVector<Domain::DocumentImage> images() const;
-    void setImages(const QVector<QPixmap>& _images);
-    Q_SIGNAL void imagesIsSet(const QVector<Domain::DocumentImage>& _images);
+    /**
+     * @brief Изображения презентации
+     */
+    QVector<QUuid> imageUuids() const;
+    QVector<QSize> imageSizes() const;
+    Q_SIGNAL void imagesChanged();
 
+    /**
+     * @brief Загрузить изображения
+     */
+    QUuid loadImagesAsync(const QVector<QUuid>& _imageUuids);
+    Q_SIGNAL void imagesLoaded(const QUuid& _queryUuid,
+                               const QVector<QPair<QUuid, QImage>>& _images);
+    Q_SIGNAL void imagesLoadingFailed();
+
+    /**
+     * @brief Разместить изображения из zip-архива во временные файлы
+     */
+    void storeToTempFiles(const QByteArray& _zipArchive);
+    Q_SIGNAL void tempFilesStored();
+
+    /**
+     * @brief Изображение было обновлено
+     */
+    Q_SIGNAL void imageUpdated(const QUuid& _uuid, const QPixmap& _image);
+
+    /**
+     * @brief Запрос на загрузку презентации
+     */
     Q_SIGNAL void downloadPresentationRequested(const QString& _filePath);
 
 protected:
