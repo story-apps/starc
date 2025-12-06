757a342f102afca14cf2bf390c77e52a61fb84d4
Ilya Putilin <putilya16@gmail.com>
2025-12-06 16:00:55 +0700

    Fix crashpad paths

--- src/3rd_party/crashpad_paths/crashpad_paths.h
+++ src/3rd_party/crashpad_paths/crashpad_paths.h
@@ -6,17 +6,16 @@ class CrashpadPaths
 {
     QString m_exeDir;
 
-    public:
-        CrashpadPaths();
-        QString getAttachmentPath();
-        QString getHandlerPath();
-        QString getReportsPath();
-        QString getMetricsPath();
-        #if defined(Q_OS_UNIX)
-            static std::string getPlatformString(QString string);
-        #elif defined(Q_OS_WINDOWS)
-            static std::wstring getPlatformString(QString string);
-        #else
-            #error getPlatformString not implemented on this platform
-        #endif
+public:
+    CrashpadPaths();
+    QString getHandlerPath();
+    QString getReportsPath();
+    QString getMetricsPath();
+#if defined(Q_OS_UNIX)
+    static std::string getPlatformString(QString string);
+#elif defined(Q_OS_WINDOWS)
+    static std::wstring getPlatformString(QString string);
+#else
+#error getPlatformString not implemented on this platform
+#endif
 };
