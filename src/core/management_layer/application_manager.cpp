1d861f56672ebabd31a1f368dbc60ba6b30d5d60
dimkanovikov <to@dimkanovikov.pro>
2026-03-05 07:23:16 +0300

    Bump version number to 0.8.2

--- src/core/management_layer/application_manager.cpp
+++ src/core/management_layer/application_manager.cpp
@@ -483,7 +483,7 @@ void ApplicationManager::Implementation::initLogging()
 #endif
     Log::init(loggingLevel, logFilePath);
 
-    QString applicationVersion = "0.8.1";
+    QString applicationVersion = "0.8.2";
 #if defined(DEV_BUILD) && DEV_BUILD > 0
     applicationVersion += QString(" dev %1").arg(DEV_BUILD);
 #endif
