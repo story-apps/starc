88d39702862570896f60e82b3616b3e065228c91
dimkanovikov <to@dimkanovikov.pro>
2026-07-20 18:53:37 +0300

    Improve eighths helper

--- src/corelib/utils/helpers/eighths_helper.cpp
+++ src/corelib/utils/helpers/eighths_helper.cpp
@@ -12,32 +12,19 @@ QString EighthsHelper::toString(qreal _eighths, bool _withPostfix)
         ++eighths;
     }
 
+    //: Page in pages scene duration
+    const auto postfix = _withPostfix ? " " + QObject::tr("p.", "EightsHelper") : "";
+
     constexpr int eight = 8;
     if (eighths < eight) {
-        return QString("%1/%2%3").arg(eighths).arg(eight).arg(
-            _withPostfix ? " "
-                    //: Page in pages scene lenght when lenght less then 1 page (1/8 ... 7/8)
-                    + QObject::tr("page", "EightsHelper")
-                         : "");
+        return QString("%1/%2%3").arg(eighths).arg(eight).arg(postfix);
     }
 
     if (eighths % eight == 0) {
-        if (_withPostfix) {
-            //: Page in pages scene lenght when lenght equal full number of pages (1 ... 99)
-            return QObject::tr("%n pages", "EightsHelper", eighths / eight);
-        }
-
-        return QString("%1").arg(eighths / eight);
+        return QString("%1%2").arg(eighths / eight).arg(postfix);
     }
 
-    return QString("%1 %2/%3%4")
-        .arg(eighths / eight)
-        .arg(eighths % eight)
-        .arg(eight)
-        .arg(_withPostfix ? " "
-                     //: Pages in pages scene lenght when lenght more then 1 page (1 1/8 ... 99 7/8)
-                     + QObject::tr("pages", "EightsHelper")
-                          : "");
+    return QString("%1 %2/%3%4").arg(eighths / eight).arg(eighths % eight).arg(eight).arg(postfix);
 }
 
 QString EighthsHelper::toStringWithPostfix(qreal _eighths)
