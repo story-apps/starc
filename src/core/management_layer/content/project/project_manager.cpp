8aa5db9f266a31e75cac85b983d826a84f53a20c
dimkanovikov <to@dimkanovikov.pro>
2026-09-19 14:15:10 +0300

    Fix crash on update view editing mode while not all drafts were loaded

--- src/core/management_layer/content/project/project_manager.cpp
+++ src/core/management_layer/content/project/project_manager.cpp
@@ -2238,16 +2238,20 @@ void ProjectManager::Implementation::updateViewsEditingMode()
     if (auto activeView = activeDocumentView(); activeView != nullptr) {
         auto item = projectStructureModel->itemForIndex(view.activeIndex);
         if (view.active->currentDraft() > 0) {
-            const auto draftIndex = view.active->currentDraft() - 1;
-            item = item->drafts().at(draftIndex);
+            if (const auto draftIndex = view.active->currentDraft() - 1;
+                item->drafts().size() > draftIndex) {
+                item = item->drafts().at(draftIndex);
+            }
         }
         activeView->setEditingMode(documentEditingMode(item));
     }
     if (auto inactiveView = inactiveDocumentView(); inactiveView != nullptr) {
         auto item = projectStructureModel->itemForIndex(view.inactiveIndex);
         if (view.active->currentDraft() > 0) {
-            const auto draftIndex = view.inactive->currentDraft() - 1;
-            item = item->drafts().at(draftIndex);
+            if (const auto draftIndex = view.active->currentDraft() - 1;
+                item->drafts().size() > draftIndex) {
+                item = item->drafts().at(draftIndex);
+            }
         }
         inactiveView->setEditingMode(documentEditingMode(item));
     }
